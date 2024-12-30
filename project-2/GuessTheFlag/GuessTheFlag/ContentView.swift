07834f9fa27cf91ffb84f46e15bb1f084439194f
Shawn Schwartz <s@shawnts.com>
2024-12-29 19:57:47 -0800

    Add(project 2): completed bonus challenges

--- project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,14 +15,27 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
 
+    // challenge 1:
+    // Add an @State property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert and in the score label.
+    @State private var userScore = 0
+
+    // challenge 2:
+    // When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example.
+
+    // challenge 3:
+    // Make the game show only 8 questions, at which point they see a final alert judging their score and can restart the game.
+    let maxNumQuestions = 8
+    @State private var currentQuestionNum = 0
+    @State private var gameOver = false
+
     var body: some View {
         ZStack {
-//            LinearGradient(
-//                colors: [.blue, .black],
-//                startPoint: .top,
-//                endPoint: .bottom
-//            )
-//                .ignoresSafeArea()
+            //            LinearGradient(
+            //                colors: [.blue, .black],
+            //                startPoint: .top,
+            //                endPoint: .bottom
+            //            )
+            //                .ignoresSafeArea()
 
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
@@ -30,7 +43,7 @@ struct ContentView: View {
             ], center: .top, startRadius: 200, endRadius: 700)
             .ignoresSafeArea()
 
-//            VStack(spacing: 30) {
+            //            VStack(spacing: 30) {
             VStack {
                 Spacer()
 
@@ -41,13 +54,13 @@ struct ContentView: View {
                     VStack {
                         // instructions for player
                         Text("Tap the flag of")
-//                            .foregroundStyle(.white)
+                        //                            .foregroundStyle(.white)
                         // use iOS vibrancy effect
                             .foregroundStyle(.secondary)
                             .font(.subheadline.weight(.heavy))  // dynamic type for accessibility (test with environment overrides when running the simulator)
 
                         Text(countries[correctAnswer])
-//                            .foregroundStyle(.white)
+                        //                            .foregroundStyle(.white)
                             .font(.largeTitle.weight(.semibold))
                     }
 
@@ -70,9 +83,13 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
 
-                Text("Score: ???")
+                Text("Score: \(userScore)/\(maxNumQuestions)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
+
+                Text("Question: \(currentQuestionNum)/\(maxNumQuestions)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold().italic())
 
                 Spacer()
             }
@@ -81,23 +98,43 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("continue", action: askQuestion)
         } message: {
-            Text("your score is ???")
+            Text("your score is: \(userScore)/\(maxNumQuestions)")
         }
+        .alert("Game Over!", isPresented: $gameOver) {
+            Button("Play again!", role: .cancel, action: reloadGame)
+        } message: {
+            Text("your final score is: \(userScore)/\(maxNumQuestions)")
+        }
+
     }
 
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That’s the flag of \(countries[number])!"
         }
 
-        showingScore = true
+        currentQuestionNum += 1
+        print(currentQuestionNum)
+
+        if currentQuestionNum == maxNumQuestions {
+            gameOver.toggle()
+        } else {
+            showingScore.toggle()
+        }
     }
 
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+
+    func reloadGame() {
+        currentQuestionNum = 0
+        userScore = 0
+        askQuestion()
     }
 }
 
